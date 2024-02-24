dbcb46919f2316281b78606d3eb06a4f5c21d24f
EgorFokin <egor.fokin2017@gmail.com>
2024-02-24 15:18:10 -0800

    updated assets

--- backend/ws.py
+++ backend/ws.py
@@ -3,7 +3,10 @@ import websockets
 import json
 import game
 import threading
-from datetime import datetime
+from datetime import datetime, timedelta
+
+
+ip_requests = {}
 
 
 async def recieve_messages(websocket):
@@ -11,16 +14,29 @@ async def recieve_messages(websocket):
         data = json.loads(message)
         if data["type"] == "join":
             if data["player_name"] not in game.queue:
+                if (not websocket.remote_address[0] in ip_requests):
+                    ip_requests[websocket.remote_address[0]] = []
+                else:
+                    for i in range(len(ip_requests[websocket.remote_address[0]])-1, -1, -1):
+                        if (datetime.now() - ip_requests[websocket.remote_address[0]][i]) > timedelta(seconds=30):
+                            ip_requests[websocket.remote_address[0]].pop(i)
+                ip_requests[websocket.remote_address[0]].append(datetime.now())
+                if len(ip_requests[websocket.remote_address[0]]) > 2:
+                    continue
                 game.queue.append(data["player_name"])
 
 
 async def send_messages(websocket):
     while True:
-        state = game.state
-        state['queue'] = game.queue
-        message = json.dumps(state)
-        await asyncio.sleep(0.01)
-        await websocket.send(message)
+        try:
+            state = game.state
+            state['queue'] = game.queue
+            message = json.dumps(state)
+            await asyncio.sleep(0.01)
+            await websocket.send(message)
+        except websockets.exceptions.ConnectionClosed:
+            del ip_requests[websocket.remote_address[0]]
+            break
 
 
 async def handler(websocket):
